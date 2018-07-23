0f33c98a4050aadbbd9a5a21f64ba867c372f320
Jeremy <jeremy@jsrath.com>
2018-07-23 21:38:04 +0200

    Initial commit for ingatlan mockup

---
+++ README.md
@@ -0,0 +1,8 @@
+## Ingatlan page mockup
+
+A demonstration of my frontend skills by recreating the Ingatlan real estate website: [https://realestatehungary.hu/](https://realestatehungary.hu/).
+
+CSS is Flexbox with a unique JavaScript menu. 
+
+A live demo is available here: [http://projects.jsrath.com/ingatlan](http://projects.jsrath.com/ingatlan). 
+
---
+++ script.js
@@ -0,0 +1,59 @@
+/* Show menu on click 
+ --------------------------------------------------- */
+const search = document.querySelector('#search');
+const menu = document.querySelector('.tab-menu-container');
+
+window.addEventListener('click', function (event) {
+  if (event.target === search || menu.contains(event.target)) {
+    menu.classList.add('visible');
+  } else {
+  }
+});
+
+/* Run a function on menu tabs click
+ --------------------------------------------------- */
+document.querySelector('.tabbar').addEventListener('click', function (event) {
+  if (event.target.className === 'tabitems') {
+    getActiveTab(event);
+  }
+});
+
+
+function getActiveTab(event) {
+  const submenu = document.querySelectorAll('.submenu');
+  const tabitems = document.querySelectorAll('.tabitems');
+
+  /* Remove all menus from view
+  --------------------------------------------------- */
+
+  for (let i = 0; i < submenu.length; i++) {
+    submenu[i].classList.remove('visible');
+  }
+
+  /* Remove the active color from all tabs
+  --------------------------------------------------- */
+  for (let i = 0; i < tabitems.length; i++) {
+    tabitems[i].classList.remove('active');
+  }
+
+  /* Set the active color for the clicked tab
+  --------------------------------------------------- */
+  event.target.classList.add('active');
+
+
+  /* Set the menu to display based on the clicked tab
+  --------------------------------------------------- */
+  document.getElementById(event.target.name).classList.add('visible');
+}
+
+/* Show clicked location in search bar
+ --------------------------------------------------- */
+const placeLinks = document.querySelectorAll('.submenu a')
+
+for (let i = 0; i < placeLinks.length; i++) {
+  placeLinks[i].addEventListener('click', function (event) {
+    let li = document.createElement('li');
+    li.innerHTML = event.target.innerHTML;
+    document.getElementById('selected-options').appendChild(li);
+  });
+}
